c820d24976f274f08c713288fa5e9b5f31aa8a4d
TotalKnob <TotalKnob@users.noreply.github.com>
2021-02-09 10:17:46 +0100

    Added prepareEnvironment

--- install.sh
+++ install.sh
@@ -14,13 +14,10 @@ sudo apt install -y \
 pip install wllvm
 pip install -U scikit-learn
 
+./prepareEnvironment.sh
+
 # Build and install muse, AFL and QSYM
 ./set.sh
 
 ./Docker/docker_build_muse.sh
-
-# AFL specific requirements
-echo core | sudo tee /proc/sys/kernel/core_pattern
-# Governor presumably only required on laptops
-#cd /sys/devices/system/cpu && echo performance | sudo tee cpu*/cpufreq/scaling_governor
 
---
+++ prepareEnvironment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# AFL specific requirements
+echo core | sudo tee /proc/sys/kernel/core_pattern
+# Governor presumably only required on laptops
+#cd /sys/devices/system/cpu && echo performance | sudo tee cpu*/cpufreq/scaling_governor
+
